de915a6d02e4341181f70bd88173b2851e695763
MsZihu <mjssdn95@gmail.com>
2021-10-19 08:43:05 +0700

    Update Clubinvite.js

--- Clubinvite.js
+++ Clubinvite.js
@@ -1,8 +1,3 @@
-// add index 
-Collection.prototype.array = function() {
-  return [...this.values()]
-  }
-// command 
 const discord = module.require("discord.js");
 const leader = [
     "551161303704797185",
@@ -20,28 +15,38 @@ module.exports = {
         if (!args[1]) {
             return message.channel.send(`chưa nhập tên người mời **${message.author.username}**`)
         }
-        let nguoitag = message.mentions.members.array() 
-        let person = nguoitag[0]
-    
+        if (!args[0]) return message.channel.send("**Please Enter A User!**");
+    let user =
+      message.mentions.members.array() ||
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.guild.members.cache.find(
+        r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()
+      ) ||
+      message.guild.members.cache.find(
+        r => r.displayName.toLowerCase() === args[0].toLocaleLowerCase()
+      );
+    if (!user) return message.channel.send("**Enter A Valid User!**");
+
         //hopqua 1
         if (args[1] === '1' || args[1] === 'club1') {
-            return message.channel.send(`${message.author.username} đã mời ${person.user.username} vào club`)
+            return message.channel.send(`${message.author.username} đã mời vào club`)
             .then(async msg => {
                 await msg.react('✅');
                 await msg.react(`❌`);
                 const filter = (reaction, user) => (reaction.emoji.name === '❌'|| reaction.emoji.name === '✅') 
-                && (user.id === nguoitag[0].id);
+                && (user.id == args[0].id);
                 msg.awaitReactions(filter, { max: 1 })
                     .then((collected) => {
                         collected.map((emoji) => {
                             switch (emoji._emoji.name) {
                                 case '✅':
                                     msg.reactions.removeAll()
-                                    message.channel.send(`<a:pickyes:880551847193092176>| **${person.user.username}** đã đồng ý tham gia club**`)
+                                    message.channel.send(`<a:pickyes:880551847193092176>| đã đồng ý tham gia club**`)
                                     break;
                                 case '❌':
                                     msg.reactions.removeAll()
-                                    message.channel.send(`<a:pickno:880551844814925836> | **${message.author.username}** đã từ chối tham gia club **${person.user.username}**`);
+                                    message.channel.send(`<a:pickno:880551844814925836> | **${message.author.username}**  đã từ chối tham gia club`);
                                     break;
                             }
                         })
